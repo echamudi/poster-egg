6bf43a0da070001d1d127a4715009ef90c66f707
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2017-04-02 05:54:32 +0700

    Add rasterizehtml @types definition

---
+++ @types/rasterizehtml/index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for rasterizehtml 0.1
+// Project: Posty
+// Definitions by: Hernawan Fa'iz Abdillah <abdillah.me>
+
+export as namespace rasterizehtml;
+
+export function drawDocument (
+    url: string,
+    canvas?: any,
+    options?: any
+) : any;
+
+export function drawURL (
+    url: string,
+    canvas?: any,
+    options?: any
+) : any;
+
+export function drawHTML (
+    html: any,
+    canvas?: any,
+    options?: any
+) : any;
+
