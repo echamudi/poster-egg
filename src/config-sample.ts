4f40425abcd7a9c2a85f1d7998b62c769f243ea8
Ezzat Chamudi <ezhmd@users.noreply.github.com>
2017-04-10 22:11:49 +0300

    Added config-sample.ts

---
+++ src/config-sample.ts
@@ -0,0 +1,12 @@
+/**
+ * Config file
+ * Duplicate this file and 
+ */
+
+'use strict';
+
+export const config = {
+
+    // Url of design data, including design-packs and design-assets
+    designDataApi: "http://localhost:60572/data"
+};
