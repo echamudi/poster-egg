53c7e37fa00b66f1b1e711c3316ecd61b77dec3b
Ezzat Chamudi <ezhmd@users.noreply.github.com>
2017-08-10 12:30:33 +0700

    enableprodmode

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import 'zone.js/dist/zone.js';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
