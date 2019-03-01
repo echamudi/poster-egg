d0a542f37d0db3804982120354977965454c2039
Ezzat Chamudi <chamudi.git@outlook.com>
2019-03-01 21:51:55 +0300

    Rebranding to Poster Egg

--- src/config.ts
+++ src/config.ts
@@ -2,23 +2,28 @@ let language: string;
 let watermarkLabel: string;
 let googleAnalytics: string = "X";
 
-let idHostnames: string[] = [
-    "www.desainmu.com",
-    "desainmu.com",
-    "beta.desainmu.com"
-];
+// let idHostnames: string[] = [
+//     "www.desainmu.com",
+//     "desainmu.com",
+//     "beta.desainmu.com"
+// ];
 
-if (idHostnames.indexOf(window.location.hostname) > -1) {
-    language = "id";
-    watermarkLabel = "desainmu.com"
-    window.document.title = "Desainmu 3";
-    googleAnalytics = "UA-57805922-5";
-} else {
-    language = "en";
-    watermarkLabel = "postyposter.com"
-    window.document.title = "Posty Poster";
-    googleAnalytics = "UA-57805922-6";
-}
+// if (idHostnames.indexOf(window.location.hostname) > -1) {
+//     language = "id";
+//     watermarkLabel = "desainmu.com"
+//     window.document.title = "Desainmu 3";
+//     googleAnalytics = "UA-57805922-5";
+// } else {
+//     language = "en";
+//     watermarkLabel = "postyposter.com"
+//     window.document.title = "Posty Poster";
+//     googleAnalytics = "UA-57805922-6";
+// }
+
+language = "en";
+watermarkLabel = "Posty Egg"
+window.document.title = "Posty Egg";
+googleAnalytics = "UA-57805922-6";
 
 export const config = {
 
